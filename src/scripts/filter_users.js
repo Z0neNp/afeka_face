9405b87e9d5aef657b06f7e47331fa23c4f2b228
Maxim Chanturiay <ardorm79@hotmail.co.il>
2020-08-12 20:30:27 +0300

    filter_users.js has been refactored

--- src/scripts/filter_users.js
+++ src/scripts/filter_users.js
@@ -1,67 +1,78 @@
 function handleFilterUsersInput() {
-  let element = document.getElementById("filter_users");
-  if(element) {
+  try {
+    let element = document.getElementById("filter_users");
     element.oninput = filterProvided;
+  } catch(err) {
+    throw Error("Failed to setup Friends API element.\n" + err.message);
   }
 }
 
 function filterProvided(element) {
   setTimeout(function() {
-    let filter = element.target.value;
-    if(filterLegal(filter)) {
-      if(filter == "*") {
-        filter = "";
+    try {
+      let filter = element.target.value;
+      if(filterLegal(filter)) {
+        if(filter == "*") {
+          filter = "";
+        }
+        else {
+          filter = filter.split(' ').join('%20');
+        }
+        otherUsers(filter);
       }
       else {
-        filter = filter.split(' ').join('%20');
+        throw new Error("The values in the user search can be letters, * or whitespace");
       }
-      otherUsers(credentials_container.getId(), filter);
-    }
-    else {
-      alert("The values in the user search can be letters, * or whitespaces");
-      element.target.value = "*";
-      handleFilterUsersInput();
+    } catch(err) {
+      alert("Failed to use the Friends API. More details in the console");
+      console.error(err.message);
     }
   }, 1000);
 }
 
 function filterLegal(filter) {
-  let legal_regex = new RegExp(/^\*$|^[a-zA-Z]+$|^[a-zA-Z]+\s$|^[a-zA-Z]+\s+[a-zA-Z]+$/g);
-  return filter.match(legal_regex);
+  try {
+    let legal_regex = new RegExp(/^\*$|^[a-zA-Z]+$|^[a-zA-Z]+\s$|^[a-zA-Z]+\s+[a-zA-Z]+$/g);
+    return filter.match(legal_regex);
+  } catch(err) {
+    throw new Error("filterLegal() - failed to make the validation");
+  }
 }
 
-function otherUsers(user_id, filter) {
-  let payload = credentials_container.get();
-  let xhr = new XMLHttpRequest();
-  xhr.open("POST", `/users/${user_id}/friends/new/${filter}`, true);
-  xhr.setRequestHeader("Content-Type", "application/text");
-  xhr.onload = function(e) {
-    if (xhr.readyState === 4) {
-      if (xhr.status === 200) {
-        try {
-          let response = JSON.parse(xhr.responseText);
-          alert(response["reason"] + "\n\n" + response["message"]);
-        } catch(err) {
-          updateOtherUsersList(xhr.responseText);
-          handleFilterUsersInput();
-          return;
+function otherUsers(filter) {
+  try {
+    let payload = credentials_container.get();
+    let user_id = credentials_container.getId();
+    let url = `/users/${user_id}/friends/new/${filter}`;
+    postRequest(url, payload, function(status, response_text) {
+      try {
+        if (status === 200) {
+          if(response_text[0] == "<") {
+            updateOtherUsersList(response_text);
+            handleFilterUsersInput();
+            return;
+          }
         }
-      } else {
-        alert(xhr.statusText);
+        let err_msg = "Server has refused to provide the other users for the Friends API.\n";
+        throw new Error(err_msg + response_text);
+      } catch(err) {
+        alert("Other users request from Friends API has failed. More details are in the console.");
+        console.error(err.message);
       }
-      window.location.href = "http://localhost:8000/";
-    }
-  };
-  xhr.onerror = function(e) {
-    alert(xhr.statusText);
-    window.location.href = "http://localhost:8000/";
-  };
-  xhr.send(payload);
+    });
+  } catch(err) {
+    alert("Other users request from Friends API has failed. More details in the console");
+    console.error(err.message);
+  }
 }
 
 function updateOtherUsersList(list) {
-  let element = document.getElementById("others_container");
-  element.innerHTML = list;
-  element.style.display = "none";
-  element.style.display = "block";
+  try {
+    let element = document.getElementById("others_container");
+    element.innerHTML = list;
+    element.style.display = "none";
+    element.style.display = "block";
+  } catch(err) {
+    throw new Error("updateOtherUsersList() - failed.\n" + err.message);
+  }
 }
